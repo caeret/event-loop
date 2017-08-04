a664f04999ef367d510d2b05b2dddd2c0232af18
Ren Hao <renhao2k@gmail.com>
2017-08-04 11:48:46 +0800

    fix path.

--- src/Gaemma/EventLoop/Factory.php
+++ src/Gaemma/EventLoop/Factory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Overnil\EventLoop;
+namespace Gaemma\EventLoop;
 
 class Factory
 {
--- src/Gaemma/EventLoop/LibEvLoop.php
+++ src/Gaemma/EventLoop/LibEvLoop.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Overnil\EventLoop;
+namespace Gaemma\EventLoop;
 
 use React\EventLoop\LoopInterface;
 use React\EventLoop\Tick\FutureTickQueue;
